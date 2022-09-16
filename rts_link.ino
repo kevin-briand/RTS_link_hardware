8702d3386cf34029b9cc136813b31223d5d2edbb
Kévin BRIAND <briand-kevin@hotmail.fr>
2022-09-16 09:41:05 +0200

    add MULTIPROG

--- rts_link.ino
+++ rts_link.ino
@@ -1,4 +1,3 @@
-
 #include <SomfyRTS.h>
 #include <EEPROM.h>
 
@@ -6,27 +5,45 @@
 SomfyRTS myRTS(3, TSR_RFM69); //Tx pin number, transmitter type
                               //pin number : pin connected to the transmitter DATA pin or to the DIO2 pin on RFM69
                               //transmitter type can be TSR_RFM69 or TSR_AM (for a generic AM 433.42MHZ transmitter)
-int idCover = 0;
+int idCover = 10;
+int inc = 0;
 void setup() {
 
   Serial.begin(56700);
-  Serial.println("20;00;Somfy RTS link V1;");
+  Serial.write("20;00;Somfy RTS link V1;");
   myRTS.setHighPower(true); //have to call it after initialize for RFM69HW
 
   idCover = EEPROM.read(255);
 }
 
+
+//20;12;TriState;ID=02a0aa;SWITCH=0;CMD=ON;
+//20;02;RTS;ID=669851;SWITCH=01;CMD=DOWN;
+//10;RTS;001;0;UP;
+//20;01;CMD UNKNOWN;
+
 void loop() {
     if(Serial.available()) { //wait command
         String value = Serial.readString();
         if(value.indexOf("10") == 0 && value.indexOf("rts") > 0) { //if command is RTS
+            inc++;
+            String zero = "";
+            if(inc < 10) zero = "0";
             
-            if(value.indexOf("PROG") > 0) { //Add Cover
-                Serial.println("20;01;OK;");
-                myRTS.sendSomfy(idCover, PROG);
-                Serial.println("20;01;RTS;ID=" + String(idCover) + ";SWITCH=0;CMD=DOWN;");//Send new cover
-                idCover++;
-                EEPROM.write(255, idCover);          
+            if(value.indexOf("PROG") > 0) { //Add cover
+              int id = idCover;
+              if(value.indexOf("MULTIPROG") > 0){
+                id = value.substring(value.indexOf(";",5)+1,value.indexOf(";",5)+4).toInt(); //Find cover ID
+              } 
+                Serial.write(String("20;" + zero + String(inc) + ";OK;").c_str());
+                myRTS.sendSomfy(id, PROG);
+                String str = "20;01;RTS;ID=" + zero + String(id) + ";SWITCH=0;CMD=DOWN;";
+                Serial.write(str.c_str());//Send new cover
+                
+                if(value.indexOf("MULTIPROG") == -1){
+                  idCover++;
+                  EEPROM.write(255, idCover);     
+                }     
             }
             else if(value.indexOf("UP") || value.indexOf("DOWN") || value.indexOf("STOP")) { //Exec CMD
               int id = value.substring(value.indexOf(";",5)+1,value.indexOf(";",5)+4).toInt(); //Find cover ID
@@ -38,17 +55,27 @@ void loop() {
               else cmd = STOP;
 
               //Send command
-              myRTS.sendSomfy(id, cmd);
-              Serial.println("20;01;OK;");
+              if(myRTS.sendSomfy(id, cmd))
+                Serial.write(String("20;" + zero + String(inc) + ";OK;").c_str());
             }
             else { //Send error
-              Serial.println("20;01;CMD UNKNOWN;");
+              Serial.write(String("20;" + zero + String(inc) + ";CMD UNKNOWN;").c_str());
             }
         }
-        else if(value.indexOf("RESET") == 0) { //Clear EEPROM
-              EEPROM.write(255,0);
-              Serial.println("EEPROM RESET");
+        else if(value.indexOf("RESET") >= 0) { //Clear EEPROM
+          for(int i=0;i<255;i++) {
+            EEPROM.write(i,0);
+          }
+          EEPROM.write(255,10);
+          Serial.println("EEPROM RESET");
+        }
+        else if(value.indexOf("REMOVE") > 0) {
+          for(int i=0;i<256;i++) {
+            myRTS.sendSomfy(i, PROG);
+          }
+        }
+        else {
+          Serial.write(String("20;ERROR").c_str());
         }
     }
-  delay(50);
 }
